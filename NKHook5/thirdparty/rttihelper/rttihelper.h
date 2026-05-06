7ea359d56aeb40751393275bcaa16de6fe910ff0
Towertest19 <towertest19@gmail.com>
2026-05-06 19:10:02 +0100

    Fix BTD5 v4.7 runtime signatures

--- NKHook5/thirdparty/rttihelper/rttihelper.h
+++ NKHook5/thirdparty/rttihelper/rttihelper.h
@@ -6,6 +6,8 @@
 #include <Windows.h>
 
 #include <cstdint>
+#include <algorithm>
+#include <cstring>
 #include <iomanip>
 #include <sstream>
 #include <string>
@@ -40,24 +42,77 @@ namespace h_rtti
 		// Convert the address over to an IDA pattern string
 		const std::string ida_pattern = bytes_to_ida_pattern((byte*)&address, 4);
 
-		// Get the end of the section (in our case the end of the .rdata section)
 		const uintptr_t end = start + size;
 		while (start && start < end)
 		{
-			auto xref = (uintptr_t)NKHook5::Utils::FindPattern(start, start + size, ida_pattern);
+			const auto xref = static_cast<uintptr_t>(NKHook5::Utils::FindPattern(start, end, ida_pattern));
 
 			// If the xref is 0 it means that there either were no xrefs, or there are no remaining xrefs.
 			// So we should break out of our loop, otherwise it will keep on trying to look for xrefs.
 			if (!xref)
 				break;
 
-			// We've found an xref, save it in the vector, and add 4 to start, so it wil now search for xrefs
-			// from the previously found xref untill we're at the end of the section, or there aren't any xrefs left.
+			// We've found an xref, save it in the vector, and add 4 to start, so it will now search for xrefs
+			// from the previously found xref until we're at the end of the section, or there aren't any xrefs left.
 			xrefs.push_back(xref);
 			start = xref + 4;
 		}
 
 		return xrefs;
+	}
+
+	struct section_info
+	{
+		uintptr_t start;
+		uintptr_t size;
+	};
+
+	std::vector<section_info> get_rtti_sections(uintptr_t base_address)
+	{
+		std::vector<section_info> sections = {};
+
+		const auto dos_header = (PIMAGE_DOS_HEADER)base_address;
+		if (dos_header->e_magic != IMAGE_DOS_SIGNATURE)
+			return sections;
+
+		const auto nt_headers = (PIMAGE_NT_HEADERS32)(base_address + dos_header->e_lfanew);
+		if (nt_headers->Signature != IMAGE_NT_SIGNATURE)
+			return sections;
+
+		PIMAGE_SECTION_HEADER section_header = IMAGE_FIRST_SECTION(nt_headers);
+		uint16_t number_of_sections = nt_headers->FileHeader.NumberOfSections;
+		while (number_of_sections > 0)
+		{
+			char section_name[IMAGE_SIZEOF_SHORT_NAME + 1] = {};
+			std::memcpy(section_name, section_header->Name, IMAGE_SIZEOF_SHORT_NAME);
+
+			const bool is_rtti_data =
+				std::strcmp(section_name, ".rdata") == 0 ||
+				std::strcmp(section_name, ".data") == 0;
+
+			if (is_rtti_data)
+			{
+				const uintptr_t section_size = std::max<uintptr_t>(section_header->Misc.VirtualSize, section_header->SizeOfRawData);
+				sections.push_back({ base_address + section_header->VirtualAddress, section_size });
+			}
+
+			section_header++;
+			number_of_sections--;
+		}
+
+		return sections;
+	}
+
+	uintptr_t find_pattern_in_rtti_sections(uintptr_t base_address, const std::string& ida_pattern)
+	{
+		for (const auto& section : get_rtti_sections(base_address))
+		{
+			const uintptr_t result = NKHook5::Utils::FindPattern(section.start, section.start + section.size, ida_pattern);
+			if (result)
+				return result;
+		}
+
+		return 0;
 	}
 
 	bool get_section_info(uintptr_t base_address, const std::string& section_name, uintptr_t& section_start, uintptr_t& section_size)
@@ -98,7 +153,7 @@ namespace h_rtti
 		// Convert the string to an IDA pattern so that we can pattern scan it
 		std::string ida_pattern = bytes_to_ida_pattern((byte*)type_descriptor_name.data(), type_descriptor_name.size());
 
-		auto rtti_type_descriptor = (uintptr_t)NKHook5::Utils::FindPattern(ida_pattern);
+		auto rtti_type_descriptor = find_pattern_in_rtti_sections(base_address, ida_pattern);
 		if (!rtti_type_descriptor) {
 			return 0;
 		}
@@ -106,13 +161,14 @@ namespace h_rtti
 		// We're doing - 0x8 here, because the location of the rtti_type_descriptor is 0x8 bytes before the string (open up client_panorama.dll in IDA and take a look) 
 		rtti_type_descriptor -= 0x8;
 
-		// We only need to get xrefs that are inside the .rdata section (there sometimes are xrefs in .text, so we have to filter them out)
-		uintptr_t rdata_start = 0, rdata_size = 0;
-		if (!get_section_info(base_address, ".rdata", rdata_start, rdata_size))
-			return 0;
+		std::vector<uintptr_t> xrefs = {};
+		for (const auto& section : get_rtti_sections(base_address))
+		{
+			const auto section_xrefs = get_xrefs_to(rtti_type_descriptor, section.start, section.size);
+			xrefs.insert(xrefs.end(), section_xrefs.begin(), section_xrefs.end());
+		}
 
 		// Get all xrefs to the type_descriptor
-		const std::vector<uintptr_t> xrefs = get_xrefs_to(rtti_type_descriptor, rdata_start, rdata_size);
 		for (const uintptr_t xref : xrefs)
 		{
 			// xref - 0x8 = offset of this vtable in complete class (from top)
@@ -129,11 +185,11 @@ namespace h_rtti
 				// Convert the object locator address to an IDA pattern
 				ida_pattern = bytes_to_ida_pattern((byte*)&object_locator, 4);
 
-				const size_t vtable_address = NKHook5::Utils::FindPattern(rdata_start, rdata_start + rdata_size, ida_pattern.c_str()) + 0x4;
-
-				// Here I'm checking for <= 4 as we're adding 0x4 to it. So if the pattern scan returns 0 we still head the fuck out
-				if (vtable_address <= 4)
+				const uintptr_t object_locator_xref = find_pattern_in_rtti_sections(base_address, ida_pattern);
+				if (!object_locator_xref)
 					return 0;
+
+				const size_t vtable_address = object_locator_xref + 0x4;
 
 				// We've now found the vtable address, however, we probably want a pointer to the vtable (which is in .text).
 				// To do this, we're going to find a reference to the vtable address, and use that as pointer.
