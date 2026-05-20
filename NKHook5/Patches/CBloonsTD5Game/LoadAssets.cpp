6271c2d986cef0a65b16c82d4b61eb1ad0c435a4
Bloonicious <155857894+Bloonicious@users.noreply.github.com>
2026-05-20 01:53:45 +0100

    Fix mutable buffer type for Extension::UseData in LoadAssets

--- NKHook5/Patches/CBloonsTD5Game/LoadAssets.cpp
+++ NKHook5/Patches/CBloonsTD5Game/LoadAssets.cpp
@@ -74,7 +74,7 @@ namespace NKHook5
                     Print(LogLevel::WARNING, "StatusDefinitions preload skipped: extension(s) unavailable");
                 }
 
-                auto loadJsonDefinitions = [](const char* phaseLog, const char* folderPrefix, auto* ext)
+                auto loadJsonDefinitions = [](const char* phaseLog, const char* folderPrefix, Extension* ext)
                 {
                     if (!ext)
                         return;
@@ -95,7 +95,7 @@ namespace NKHook5
                             continue;
 
                         Print(LogLevel::INFO, "Loading JSON definition from: %s", entry.c_str());
-                        const auto data = zipBase.ReadEntry(entry);
+                        auto data = zipBase.ReadEntry(entry);
                         if (!data.empty())
                             ext->UseData(data.data(), data.size());
                     }
@@ -105,22 +105,13 @@ namespace NKHook5
                 loadJsonDefinitions(
                     "Hijacking lab definitions for dynamic max levelling...",
                     "Assets/JSON/LabDefinitions/",
-                    ExtensionManager::Get<LabDefinitionsExt>());
+                    ExtensionManager::Get<NKHook5::Extensions::LabDefinitions::LabDefinitionsExt>());
 
                 loadJsonDefinitions(
                     "Hijacking specialty definitions for dynamic max levelling...",
                     "Assets/JSON/SpecialtyDefinitions/",
-                    ExtensionManager::Get<SpecialtyDefinitionsExt>());
+                    ExtensionManager::Get<NKHook5::Extensions::SpecialtyDefinitions::SpecialtyDefinitionsExt>());
 
-                LoadDefinitionsFromArchive(
-                    "Hijacking lab definitions for dynamic max levelling...",
-                    "Assets/JSON/LabDefinitions/",
-                    ExtensionManager::Get<LabDefinitionsExt>());
-
-                LoadDefinitionsFromArchive(
-                    "Hijacking specialty definitions for dynamic max levelling...",
-                    "Assets/JSON/SpecialtyDefinitions/",
-                    ExtensionManager::Get<SpecialtyDefinitionsExt>());
 
                 Print(LogLevel::INFO, "Custom assets loaded!");
 
